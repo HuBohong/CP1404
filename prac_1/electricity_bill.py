c289705e06f5eabc8c727ff8654edea4cc9b9029
HuBohong <bohong.hu@my.jcu.edu.au>
2025-09-22 09:27:56 +0800

    Practical one spell change

--- prac_1/electricity_bill.py
+++ prac_1/electricity_bill.py
@@ -12,6 +12,8 @@ Enter daily use in kWh: 4.5
 Enter number of billing days: 90
 Estimated bill: $141.75
 """
+from prac_1.sequences import user_choice
+
 TARIFF_11 = 0.244618
 TARIFF_31 = 0.136928
 electric_price = int(input("Enter cents per kWh:"))
@@ -26,12 +28,14 @@ while electric_price < 0 or daily_use < 0 or billing_days < 0:
     billing_days = int(input("Enter number of billing days:"))
 
 # total cost calcu
+user_TARIFF = 0
 tariff_choice = int(input("Which tariff? 11 or 31:"))
 if tariff_choice == 11:
-    total_cost = electric_price * daily_use * billing_days * TARIFF_11
+    user_TARIFF = TARIFF_11
 elif tariff_choice == 31:
-    total_cost = electric_price * daily_use * billing_days * TARIFF_31
+    user_TARIFF = TARIFF_31
 else:
     print("Invalid input")
 
+total_cost = electric_price * daily_use * billing_days * user_TARIFF
 print(f"the total billing is {total_cost}")

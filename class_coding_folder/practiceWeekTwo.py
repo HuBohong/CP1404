9b05c178aec8e7244ea3337731319d340d6a4395
HuBohong <bohong.hu@my.jcu.edu.au>
2025-09-27 09:48:44 +0800

    Practice file open and write

---
+++ class_coding_folder/practiceWeekTwo.py
@@ -0,0 +1,21 @@
+# with open("GuitarPractice" , "r") as file:
+#     for line in file:
+#         word = line.strip().split(",")
+#         # print(word)
+#         try:
+#             print(f"Guitar {word[0]} founded in {word[1]} price is ${word[2]}")
+#         except IndexError:
+#             # print("Invalid line in file")
+#             word.append("Unknown")
+#             word.append("0")
+#             print(f"Guitar {word[0]} founded in {word[1]} price is ${word[2]}")
+
+# with open("GuitarPractice", "a") as file:
+#     # print()
+#     file.write("abc,2020,1000 \n")
+
+with open("GuitarPractice" , "r") as file:
+    # line = file.readline()
+    # print(line)
+    line = file.readlines()
+    print(line)
